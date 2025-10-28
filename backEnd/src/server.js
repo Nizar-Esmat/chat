483b89daab413cf275347faa808a4f294602ca9a
Nizar-Esmat <nizaresmat2000@gmail.com>
2025-10-29 01:43:17 +0300

    refactor: reorganize server.js and add package.json for project setup

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -1,14 +1,27 @@
 import express from "express"
-const app = express();
 import dontenv from "dotenv"
+import path from "path"
 
+
+
+const app = express();
+const __dirname = path.resolve();
 dontenv.config()
 
 const PORT = process.env.PORT || 3000;
 
 import authRoutes from "./routes/auth.route.js"
-app.use("api/auth" , authRoutes)
+app.use("api/auth", authRoutes)
 
+
+
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname, "../../frontEnd/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontEnd/dist/index.html"))
+    })
+}
 app.listen(PORT, () => {
     console.log(`server is runing on port ${PORT}`)
 }) 
