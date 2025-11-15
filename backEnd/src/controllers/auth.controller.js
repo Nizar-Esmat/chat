fa9f9dd9429b77dcccdce9561df04c7a43a7506b
Nizar-Esmat <nizaresmat2000@gmail.com>
2025-11-15 17:15:59 +0200

    fix: reorder user creation logic to ensure token generation occurs after saving the user

--- backEnd/src/controllers/auth.controller.js
+++ backEnd/src/controllers/auth.controller.js
@@ -20,9 +20,8 @@ export const signUp = async (req, res) => {
         })
 
         if (newUser) {
-            generateToken(newUser._id , res)
             await newUser.save()
-
+            generateToken(newUser._id , res)
             res.status(201).json({
                 id: newUser._id,
                 fullName: newUser.fullName,
