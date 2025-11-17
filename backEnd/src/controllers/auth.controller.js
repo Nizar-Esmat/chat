676dfb1153572fc1552d30d31bb54b82ff8fcd0e
Nizar-Esmat <nizaresmat2000@gmail.com>
2025-11-17 11:05:39 +0200

    feat: implement signIn and logOut functionality in auth routes

--- backEnd/src/controllers/auth.controller.js
+++ backEnd/src/controllers/auth.controller.js
@@ -1,4 +1,3 @@
-
 import { validationResult } from "express-validator";
 import bycrypt from "bcryptjs"
 import User from "../models/user.model.js";
@@ -34,10 +33,10 @@ export const signUp = async (req, res) => {
             })
 
 
-            try{
+            try {
                 console.log(newUser.email);
                 await sendWelcomeEmail(newUser.email, newUser.fullName, process.env.CLIENT_URL);
-            }catch(err){
+            } catch (err) {
                 console.log(err);
             }
 
@@ -48,4 +47,42 @@ export const signUp = async (req, res) => {
         console.log("error in  signUp controller");
         res.status(500).json({ msg: "internal server error" })
     }
+}
+
+export const signIn = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const result = validationResult(req)
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() })
+        }
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ errors: [{ msg: "invalid credentials" }] })
+        }
+
+        const isMatch = await bycrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({ errors: [{ msg: "invalid credentials" }] })
+        }
+
+        generateToken(user._id, res)
+        res.status(200).json({
+            msg: "user signed in",
+            user: user
+        })
+
+    } catch (err) {
+        console.log("error in  signIn controller", err);
+        res.status(500).json({ msg: "internal server error" })
+    }
+}
+
+export const logOut = (_, res) => {
+    res.cookie("jwt", "", {
+        maxAge: 0
+    });
+    res.status(200).json({ msg: "user logged out" })
 }
